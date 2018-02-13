51d179b73f5fc3bfe7d2f6c830f016fe02b3d36f
Leonid Makarov <leonid.makarov@ffwagency.com>
2018-02-12 17:17:53 -0800

    Added Varnish 5.2

---
+++ 5.2/startup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Delay start to avoid DNS lookup issues
+echo 'Waiting 5s before startup...'
+sleep 5
+
+echo 'Copying config from /opt/default.vcl...'
+cp -f /opt/default.vcl /etc/varnish/default.vcl
+
+echo 'Evaluating config variables...'
+for name in VARNISH_BACKEND_PORT VARNISH_BACKEND_HOST VARNISH_BACKEND_DOMAIN
+do
+    eval value=\$$name
+    sed -i "s|{${name}}|${value}|g" /etc/varnish/default.vcl
+done
+
+echo 'Starting varnishd...'
+varnishd -f /etc/varnish/default.vcl \
+	-s malloc,${VARNISH_CACHE_SIZE} \
+	-a :${VARNISH_PORT} \
+	-T :${VARNISH_ADMIN_PORT} \
+	${VARNISH_VARNISHD_PARAMS}
+
+echo 'Streaming logs (varnishncsa)...'
+varnishncsa ${VARNISH_VARNISHNCSA_PARAMS}
