32e0657ca49f8de308731ac62b3ec50e96187d14
Sergey Zabolotny <sergey.zabolotny@ffwagency.com>
2019-01-04 12:13:25 +0200

    Fix checkhealth script

--- 3.0/healthcheck.sh
+++ 3.0/healthcheck.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-netstat -nlp | grep -E 'tcp.*53.*LISTEN.*dnsmasq' >/dev/null || exit 1
-netstat -nlp | grep -E 'udp.*53.*dnsmasq' >/dev/null || exit 1
+netstat -nlp | grep -E 'tcp.*:80.*LISTEN.*' >/dev/null || exit 1
+netstat -nlp | grep -E 'tcp.*:6082.*LISTEN.*' >/dev/null || exit 1
 
 exit 0
