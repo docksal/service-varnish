de66c894ad1170fb909243f10064bb8c564a2127
Leonid Makarov <leonid.makarov@ffwagency.com>
2019-01-29 10:57:07 -0800

    Refactored release.sh to support edge image tags

--- release.sh
+++ release.sh
@@ -2,16 +2,22 @@
 
 set -e
 
-if [[ "${TRAVIS_PULL_REQUEST}" == "false" && ("${TRAVIS_BRANCH}" == "master"  || -n "${TRAVIS_TAG}") ]]; then
-    docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
+	# Image tag scheme: <software-version>-<image-stability-tag>
+	# Examples:
+	# develop => 4.1-edge, 5.2-edge
+	# master => 4.1 / 4, 5.2 / 5 / latest
+	# 1.0.0 => 4.1-1.0 / 4-1.0, 5.2-1.0 / 5-1.0
+	if [[ "${TRAVIS_BRANCH}" == "develop" ]]; then export STABILITY_TAG="edge"; fi
+	if [[ "${TRAVIS_BRANCH}" == "master" ]]; then export STABILITY_TAG=""; fi
+	if [[ "${TRAVIS_TAG}" != "" ]]; then export STABILITY_TAG="${TRAVIS_TAG:1:3}"; fi
 
-    if [[ -n "${TRAVIS_TAG}" ]]; then
-        export STABILITY_TAG="${TRAVIS_TAG}"
-    fi
+	docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}"
 
-    IFS=',' read -ra tags <<< "${TAGS}"
+	IFS=',' read -ra tags <<< "${TAGS}"
 
-    for tag in "${tags[@]}"; do
-        make release TAG="${tag}";
-    done
+	# Push all applicable tag variations
+	for tag in "${tags[@]}"; do
+		make release TAG="${tag}";
+	done
 fi
