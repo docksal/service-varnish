f5b33609c9a1f59bc1633fe9bb98c1fb5d8fdf51
Leonid Makarov <leonid.makarov@ffwagency.com>
2019-01-29 15:59:51 -0800

    Disable docker hub pushes for other branches

--- release.sh
+++ release.sh
@@ -2,15 +2,23 @@
 
 set -e
 
+# No pushes for PRs
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
 	# Image tag scheme: <software-version>-<image-stability-tag>
 	# Examples:
 	# develop => 4.1-edge, 5.2-edge
 	# master => 4.1 / 4, 5.2 / 5 / latest
 	# 1.0.0 => 4.1-1.0 / 4-1.0, 5.2-1.0 / 5-1.0
-	if [[ "${TRAVIS_BRANCH}" == "develop" ]]; then export STABILITY_TAG="edge"; fi
-	if [[ "${TRAVIS_BRANCH}" == "master" ]]; then export STABILITY_TAG=""; fi
-	if [[ "${TRAVIS_TAG}" != "" ]]; then export STABILITY_TAG="${TRAVIS_TAG:1:3}"; fi
+	if [[ "${TRAVIS_BRANCH}" == "develop" ]]; then
+		export STABILITY_TAG="edge"
+	elif [[ "${TRAVIS_BRANCH}" == "master" ]]; then
+		export STABILITY_TAG=""
+	elif [[ "${TRAVIS_TAG}" != "" ]]; then
+		export STABILITY_TAG="${TRAVIS_TAG:1:3}"
+	else
+		# No pushes for any other branches
+		exit 0
+	fi
 
 	docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}"
 
@@ -18,6 +26,6 @@ if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
 
 	# Push all applicable tag variations
 	for tag in "${tags[@]}"; do
-		make release TAG="${tag}";
+		make release TAG="${tag}"
 	done
 fi
