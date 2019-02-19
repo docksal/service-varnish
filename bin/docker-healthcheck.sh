a9aa3c9dc885884371b2ea4fa347f2e3d6d2083b
Leonid Makarov <leonid.makarov@ffwagency.com>
2019-02-19 15:11:01 -0800

    Updated docker-healthcheck.sh
    - dropped the varnish config file check and it's too heavy
    - using ENV variables for VARNISH_PORT and VARNISH_ADMIN_PORT

--- bin/docker-healthcheck.sh
+++ bin/docker-healthcheck.sh
@@ -3,9 +3,6 @@
 # Fail if any of the health checks failed
 set -e
 
-# Check config
-varnishd -C -f /etc/varnish/default.vcl
-
 # Check port binding
-netstat -nlp | grep -E 'tcp.*:80.*LISTEN.*' >/dev/null
-netstat -nlp | grep -E 'tcp.*:6082.*LISTEN.*' >/dev/null
+netstat -nlp | grep -E "tcp.*:${VARNISH_PORT}.*LISTEN.*" >/dev/null
+netstat -nlp | grep -E "tcp.*:${VARNISH_ADMIN_PORT}.*LISTEN.*" >/dev/null
