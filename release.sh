9d604faeb5ec58c45864416cb8cbdc4d4e8e8bd2
Les Peabody <lpeabody@gmail.com>
2021-04-27 22:53:02 -0400

    Move to GitHub Actions.

--- release.sh
+++ release.sh
@@ -2,27 +2,37 @@
 
 set -e
 
+# --------- Helper functions --------- #
+
+# Check whether the current build is for a pull request
+is_pr ()
+{
+    [[ "${EVENT_NAME}" == "pull_request" ]]
+}
+
+# -------------------------------------#
+
 # No pushes for PRs
-if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
-	# Image tag scheme: <software-version>-<image-stability-tag>[-<flavor>]
-	if [[ "${TRAVIS_BRANCH}" == "develop" ]]; then
-		export STABILITY_TAG="edge"
-	elif [[ "${TRAVIS_BRANCH}" == "master" ]]; then
-		export STABILITY_TAG=""
-	elif [[ "${TRAVIS_TAG}" != "" ]]; then
-		export STABILITY_TAG="${TRAVIS_TAG:1:3}"
-	else
-		# No pushes for any other branches
-		exit 0
-	fi
+is_pr && exit
 
-	docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}"
-
-	IFS=',' read -ra tags <<< "${TAGS}"
-
-	# Push all applicable tag variations
-	for tag in "${tags[@]}"; do
-		if [[ "${TRAVIS_BRANCH}" == "develop" ]] && [[ "${tag}" =~ "latest" ]]; then continue; fi
-		make release TAG="${tag}"
-	done
+# Image tag scheme: <software-version>-<image-stability-tag>[-<flavor>]
+if [[ "${SOURCE_BRANCH}" == "develop" ]]; then
+    export STABILITY_TAG="edge"
+elif [[ "${SOURCE_BRANCH}" == "master" ]]; then
+    export STABILITY_TAG=""
+elif [[ "${SOURCE_TAG}" != "" ]]; then
+    export STABILITY_TAG="${SOURCE_TAG:1:3}"
+else
+    # No pushes for any other branches
+    exit 0
 fi
+
+docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}"
+
+IFS=',' read -ra tags <<< "${TAGS}"
+
+# Push all applicable tag variations
+for tag in "${tags[@]}"; do
+    if [[ "${SOURCE_BRANCH}" == "develop" ]] && [[ "${tag}" =~ "latest" ]]; then continue; fi
+    make release TAG="${tag}"
+done
